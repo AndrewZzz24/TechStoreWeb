ca10921ba136ddd8d984bd8ee2d28f956cd145d6
Andrew Zmushko <Andr.zmushko@yandex.ru>
2023-03-21 23:30:04 +0300

    fix: fix issues

--- src/serverTime.interceptor.ts
+++ src/serverTime.interceptor.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
-export class LoggingInterceptor implements NestInterceptor {
+export class ServerTimeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const now = Date.now();
     return next.handle().pipe(
