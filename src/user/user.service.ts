0e464d4f5f9b508c822170a19a333a0ad36329bf
Andrew Zmushko <Andr.zmushko@yandex.ru>
2023-04-11 22:45:32 +0300

    feat: initial commit

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,51 @@
-import { Injectable, NotImplementedException } from '@nestjs/common';
-import { UserDto } from './dto/user.dto';
+import { Injectable, NotImplementedException } from "@nestjs/common";
+import { UserDto } from "./dto/user.dto";
+import { PrismaService } from "../prisma.service";
+import { PrismaClient } from "@prisma/client";
 
+// const prisma = new PrismaClient()
 @Injectable()
 export class UserService {
-  getUser(uid: string): UserDto {
+
+  constructor(private prisma: PrismaService) {}
+
+  async getUser(
+    uid: string
+  ): Promise<UserDto> {
+    const posts = await this.prisma.user.findMany();
     throw new NotImplementedException();
   }
 
-  createUser(uid: string): UserDto {
-    throw new NotImplementedException();
+  async createUser(uid: string): Promise<UserDto> {
+
+    const siteUserData = await this.prisma.siteUserData.create({
+      data: {
+        email: "uid@techstore.ru",
+        name: uid,
+        username: uid,
+        password: "asd",
+        role: "asd"
+      }
+    });
+
+    const user = await this.prisma.user.create({
+      data: {
+        siteUserDataId: siteUserData.id
+      }
+    });
+    return new UserDto();
   }
 
-  deleteUser(uid: string): boolean {
-    throw new NotImplementedException();
+  async deleteUser(uid: string): Promise<boolean> {
+    const post = await this.prisma.user.delete({
+
+      where: {
+
+        siteUserDataId: Number(id),
+
+      },
+
+    })
+
   }
 }
