0f0307a0b51c89d25b8d7122b3929de77becd3d1
andrewzmushko <andrewzmushko@gmail.com>
2023-04-26 15:53:39 +0300

    fix: fix issues

--- public/Sources/Scripts/feedbackForm.js
+++ public/Sources/Scripts/feedbackForm.js
@@ -1,28 +1,33 @@
 function listenToSubmit() {
-  document.addEventListener("submit", (event) => {
+  document.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const title = document.getElementById("title").value.trim();
-    const description = document.getElementById("description").value.trim();
-    const userdata = JSON.parse(localStorage.getItem("userdata"));
+    const title = document.getElementById('title').value.trim();
+    const description = document.getElementById('description').value.trim();
+    const userdata = JSON.parse(localStorage.getItem('userdata'));
     if (userdata === null) {
-      alert("You have to log in first");
+      alert('You have to log in first');
       return;
     }
     let supportRequest = {
-      userId: userdata["id"], title: title, message: description
+      userId: userdata['id'],
+      title: title,
+      message: description,
     };
 
-    document.getElementById("feedback-form-id").reset();
+    document.getElementById('feedback-form-id').reset();
 
-    fetch("support/create-support-request", {
-      method: "POST", headers: {
-        "Content-Type": "application/json"
-      }, body: JSON.stringify(supportRequest)
-    }).then(response => response.json())
-      .then(data => {
-        if (data == null || data["statusCode"] === 500) {
-          alert("invalid support request");
+    fetch('support/create-support-request', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(supportRequest),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data == null || data['statusCode'] === 500) {
+          alert('invalid support request');
         } else {
           console.log(JSON.stringify(supportRequest));
           renderSuggest(data);
@@ -32,30 +37,32 @@ function listenToSubmit() {
 }
 
 function displayReviews() {
-  const userdata = JSON.parse(localStorage.getItem("userdata"));
+  const userdata = JSON.parse(localStorage.getItem('userdata'));
 
   if (userdata === null) {
-    alert("You have to log in first");
+    alert('You have to log in first');
     return;
   }
   let allData = [];
   let page = 0;
   let itemsPerPage = 2;
-  const button = document.querySelector("#downloadButton");
-  fetch("support/get-user-support-requests/" + userdata["id"], {
-    method: "POST", headers: {
-      "Content-Type": "application/json"
-    }
-  }).then(response => response.json())
-    .then(data => {
-      if (data == null || data["statusCode"] === 500) {
-        alert("invalid support request");
+  const button = document.querySelector('#downloadButton');
+  fetch('users/' + userdata['id'] + '/support-requests', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data == null || data['statusCode'] === 500) {
+        alert('invalid support request');
       } else {
         console.log(JSON.stringify(data));
         let stringified = JSON.parse(JSON.stringify(data));
         for (let i = 0; i < itemsPerPage; i++) {
           if (i >= data.length) {
-            button.style.display = "none";
+            button.style.display = 'none';
             break;
           }
           renderSuggest(data[i]);
@@ -63,42 +70,40 @@ function displayReviews() {
         allData = data;
       }
     });
-  button.addEventListener("click", (event) => {
+  button.addEventListener('click', (event) => {
     event.preventDefault();
     page += 1;
     for (let i = 0; i < itemsPerPage; i++) {
       if (i + itemsPerPage * page >= allData.length) {
-        button.style.display = "none";
+        button.style.display = 'none';
         break;
       }
       renderSuggest(allData[i + itemsPerPage * page]);
     }
     if (itemsPerPage * (page + 1) >= allData.length) {
-      button.style.display = "none";
+      button.style.display = 'none';
     }
   });
-
 }
 
 function renderSuggest(suggestObject) {
-  let tbody = document.querySelector(".grid-from-table-body");
-  let template = document.querySelector("#feedback-form-response");
+  let tbody = document.querySelector('.grid-from-table-body');
+  let template = document.querySelector('#feedback-form-response');
 
   let clone = template.content.cloneNode(true);
-  let td = clone.querySelectorAll("td");
+  let td = clone.querySelectorAll('td');
 
-  td[0].textContent = suggestObject["title"];
-  td[1].textContent = suggestObject["userId"];
-  td[2].textContent = suggestObject["message"];
-  td[3].textContent = suggestObject["status"];
+  td[0].textContent = suggestObject['title'];
+  td[1].textContent = suggestObject['userId'];
+  td[2].textContent = suggestObject['message'];
+  td[3].textContent = suggestObject['status'];
 
   tbody.appendChild(clone);
 }
 
-(function() {
-  document.addEventListener("DOMContentLoaded", () => {
+(function () {
+  document.addEventListener('DOMContentLoaded', () => {
     listenToSubmit();
     displayReviews();
   });
 })();
-
